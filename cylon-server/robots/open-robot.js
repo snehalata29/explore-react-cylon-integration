3c091dac38740ba0cb3a523b3b231bd73b396b46
Anoop Sharma <38686430+anoopsharma010395@users.noreply.github.com>
2021-06-14 19:01:29 +0530

    Create open-robot.js
    Added Open-robot.js

---
+++ cylon-server/robots/open-robot.js
@@ -0,0 +1,78 @@
+const cylon = require('cylon');
+const cylonOpencv = require('cylon-opencv');
+
+
+
+class OpenRobot {
+  constructor() {
+    this.robot = null;
+  }
+  setOpenCvRobotConfiguration() {
+    this.robot = cylon
+      .robot({
+        // Change the port to the correct port for your Arduino.
+        connections: {
+            opencv: { adaptor: 'opencv' }
+          },
+          devices: {
+            window: { driver: 'window' },
+            camera: {
+              driver: 'camera',
+              camera: 0,
+              haarcascade: '/Users/anoop.sharma/Desktop/Web applications/cylon/new-app/explore-react-cylon-integration/cylon-server/node_modules/cylon-opencv/examples/display_camera/haarcascade_frontalface_alt.xml'
+            }
+          },
+      })
+      .start();
+      this.startopencv();
+  }
+
+ 
+  startopencv() {
+      var self = this;
+      console.log('The camera is ready!' +  JSON.stringify(this.robot.devices.camera))
+        this.robot.devices.camera.once('cameraReady', function() {
+        console.log('The camera is ready!')
+  
+        // We listen for frame ready event, when triggered
+        // we display the frame/image passed as an argument
+        // and we tell the window to wait 40 milliseconds
+        var frameSelf = self;
+        //console.log('started opencv')
+        self.robot.devices.camera.on('frameReady', function(err, im) {
+
+          //console.log("FRAMEREADY!");
+          //console.log(frameSelf.robot.devices.window)
+          //console.log("FRAMEREADY!");
+
+          console.log("frameready!");
+
+          console.log(im.read());
+
+          console.log("frameready!");
+          frameSelf.robot.devices.window.show(im, 400);
+        });
+  
+        // Here we have two options to start reading frames from
+        // the camera feed.
+        // 1. As fast as possible triggering the next frame read
+        //    in the listener for frameReady, if you need video
+        //    as smooth as possible uncomment #my.camera.readFrame()
+        //    in the listener above and the one below this comment.
+        //
+        // my.camera.readFrame()
+        //
+        // 2. Use an interval of time to try and get aset amount
+        //    of frames per second  (FPS), in the next example
+        //    we are trying to get 1 frame every 50 milliseconds
+        //    (20 FPS).
+        //
+        //every(50, function() { frameSelf.robot.devices.camera.readFrame(); });
+
+        setInterval(function() { frameSelf.robot.devices.camera.readFrame(); }, 5000);
+      });
+  }
+  
+}
+
+module.exports = new OpenRobot();
